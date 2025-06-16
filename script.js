6417866076d475eead05a846c0f0475a71a61c21
Bharat <hibharatj@gmail.com>
2025-06-16 13:24:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function toCase(text) {
   // write your code here
+	let textInLower = text.toLowerCase();
+	let textInUpper = text.toUpperCase();
+
+	return `${textInLower}-${textInUpper}`
+	
 }
 
 // DO not change the code below
